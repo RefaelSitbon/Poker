b0c893037cdedd9e81ac1119e9c639447a4939ac
refael.sitbon <refael.sitbon@gmail.com>
2023-05-13 18:14:33 +0300

    add some handler function

--- Poker/src/Poker.java
+++ Poker/src/Poker.java
@@ -1,7 +1,6 @@
 package Poker.src;
 
 import java.util.*;
-import static Poker.src.Card.castStrToNum;
 import static Poker.src.PokerHandler.*;
 
 class Poker{
@@ -14,7 +13,7 @@ class Poker{
         cardsAndDeck = cards;
         initNumArr();
         initTypeArr();
-        initCardMap();
+        initCardMap(this.cardMap);
     }
 
     private  void initTypeArr(){
@@ -29,7 +28,7 @@ class Poker{
         numArr.add(CardNumEnum.AS);
     }
 
-    private void initCardMap(){
+    private void initCardMap(Map<CardTypeEnum, ArrayList<CardNumEnum>> cardMap){
         cardMap.put(CardTypeEnum.Heart, new ArrayList<>());
         cardMap.put(CardTypeEnum.Diamond, new ArrayList<>());
         cardMap.put(CardTypeEnum.Club, new ArrayList<>());
@@ -104,41 +103,60 @@ class Poker{
     }
 
     private void checkOptionCard(Map<HandRating, ArrayList<ArrayList<Card>>> ratingMap,
-                                                                        ArrayList<Card> cards){
-        if(checkStraightFlash(cards)){ratingMap.get(HandRating.StraightFlash).add((ArrayList<Card>) cards.clone());}
-        if(checkFourOfAKind(cards)){ratingMap.get(HandRating.FourOfAKind).add((ArrayList<Card>) cards.clone());}
-        if(checkFullHouse(cards)){ratingMap.get(HandRating.FullHouse).add((ArrayList<Card>) cards.clone());}
-        if(checkFlush(cards, false)){ratingMap.get(HandRating.Flush).add((ArrayList<Card>) cards.clone());}
-        if(checkStraight(createArrOfInt(cards))){ratingMap.get(HandRating.Straight).add((ArrayList<Card>) cards.clone());}
-        if(checkTriple(cards)){ratingMap.get(HandRating.Triple).add((ArrayList<Card>) cards.clone());}
-        if(checkPair(cards)){ratingMap.get(HandRating.Pair).add((ArrayList<Card>) cards.clone());}
+                                                                        ArrayList<Card> cards, Card card1, Card card2){
+        ArrayList<Card> cards1 = new ArrayList<>();
+        cards1.add(card1);cards1.add(card2);
+        if(checkStraightFlash(cards)){ratingMap.get(HandRating.StraightFlash).add(cards1);}
+        if(checkFourOfAKind(cards)){ratingMap.get(HandRating.FourOfAKind).add(cards1);}
+        if(checkFullHouse(cards)){ratingMap.get(HandRating.FullHouse).add(cards1);}
+        if(checkFlush(cards, false)){ratingMap.get(HandRating.Flush).add(cards1);}
+        if(checkStraight(createArrOfInt(cards))){ratingMap.get(HandRating.Straight).add(cards1);}
+        if(checkTriple(cards)){ratingMap.get(HandRating.Triple).add(cards1);}
+        if(checkPair(cards)){ratingMap.get(HandRating.Pair).add(cards1);}
     }
 
-    private Map<HandRating, ArrayList<ArrayList<Card>>> calculateOption(){
+    private Map<CardTypeEnum, ArrayList<CardNumEnum>> cloneCardMap(){
+        Map<CardTypeEnum, ArrayList<CardNumEnum>> newCardMap = new HashMap<>();
+        initCardMap(newCardMap);
+        for (CardTypeEnum type: cardMap.keySet()) {
+            newCardMap.put(type,(ArrayList<CardNumEnum>) cardMap.get(type).clone());
+        }
+        return newCardMap;
+    }
+
+    private Map<HandRating, ArrayList<ArrayList<Card>>> calculateOption(boolean firstHit){
         Map<HandRating, ArrayList<ArrayList<Card>>> ratingMap = new HashMap<>();
         initRateMap(ratingMap);
         ArrayList<Card> cards = (ArrayList<Card>)this.cardsAndDeck.clone();
-        Map<CardTypeEnum, ArrayList<CardNumEnum>> newDeckMap = new HashMap<>(cardMap);
-        Card card = null;
-        for (CardTypeEnum type: typeArr) {
-            for (CardNumEnum num: numArr) {
-                if(newDeckMap.get(type) != null && newDeckMap.get(type).contains(num)) {
+        Map<CardTypeEnum, ArrayList<CardNumEnum>> newDeckMap = cloneCardMap();
+        Card card1 = null;
+        Card card2 = null;
+        for (CardTypeEnum type1: typeArr) {
+            for (CardNumEnum num1: numArr) {
+                if(newDeckMap.get(type1).contains(num1)) {
                 }else {
-                    card = new Card(type, num);
-                    cards.add(card);
+                    card1 = new Card(type1, num1);
+                    cards.add(card1);
                     PokerHandler.sortCard(cards);
-                    newDeckMap.get(type).add(num);
-                    for (CardTypeEnum type1: typeArr) {
-                        for (CardNumEnum num1: numArr) {
-                            if(newDeckMap.get(type1) != null && newDeckMap.get(type1).contains(num1)) {
-                            }else {
-                                checkOptionCard(ratingMap, (ArrayList<Card>) cards.clone());
+                    newDeckMap.get(type1).add(num1);
+                    if(firstHit) {
+                        for (CardTypeEnum type2 : typeArr) {
+                            for (CardNumEnum num2 : numArr) {
+                                if (newDeckMap.get(type2).contains(num2)) {
+                                } else {
+                                    card2 = new Card(type2, num2);
+                                    cards.add(card2);
+                                    PokerHandler.sortCard(cards);
+                                    checkOptionCard(ratingMap, (ArrayList<Card>) cards.clone(), card1, card2);
+                                    cards.remove(card2);
+                                }
                             }
                         }
+                    }else {
+                        checkOptionCard(ratingMap, (ArrayList<Card>) cards.clone(), card1, null);
                     }
-                    newDeckMap.get(type).remove((CardNumEnum) num);
-
-//                    cards.remove(card);
+                    cards.remove(card1);
+                    newDeckMap.get(type1).remove(num1);
                 }
             }
         }
@@ -164,15 +182,46 @@ class Poker{
         }
     }
 
-    public void printRateDeck(HandRating handRating){
-        Map<HandRating, ArrayList<ArrayList<Card>>> map = this.calculateOption();
+    public void printRateDeck(HandRating handRating, boolean firstHit){
+        Map<HandRating, ArrayList<ArrayList<Card>>> map = calculateOption(firstHit);
         System.out.println("$$$$$$$$$ " + handRating.toString() + " $$$$$$$$$");
         for (ArrayList<Card> arr: map.get(handRating)) {
             for (Card c: arr) {
-                System.out.print(c.num + " " + c.type + ",");
+                System.out.print(c.num.ordinal() + 2 + " " + c.type + ",");
             }
             System.out.println();
         }
+    }
+
+    public void printAmountOfOption(HandRating handRating, boolean firstHit){
+        Map<HandRating, ArrayList<ArrayList<Card>>> map = calculateOption(firstHit);
+        System.out.println("######### " + handRating.toString() + " #########");
+
+        if(firstHit && map.get(handRating).size() > 0) {
+            System.out.println(map.get(handRating).size() + " from " + 2209 + " options, SUM: " + 100/(Float.intBitsToFloat(2209)/Float.intBitsToFloat(map.get(handRating).size())));
+        } else if (map.get(handRating).size() > 0) {
+            System.out.println(map.get(handRating).size() + " from " + 2116 + " options, SUM: " + 100/(Float.intBitsToFloat(2116)/Float.intBitsToFloat(map.get(handRating).size())));
+        }
+    }
+
+    public void sendToPrintEveryCardOption(boolean firstHit){
+        printRateDeck(HandRating.StraightFlash, firstHit);
+        printRateDeck(HandRating.FourOfAKind, firstHit);
+        printRateDeck(HandRating.FullHouse, firstHit);
+        printRateDeck(HandRating.Flush, firstHit);
+        printRateDeck(HandRating.Straight, firstHit);
+        printRateDeck(HandRating.Triple, firstHit);
+//        printRateDeck(HandRating.Pair);
+    }
+
+    private void sendToPrintEveryRate(boolean firstHit){
+        printAmountOfOption(HandRating.StraightFlash, firstHit);
+        printAmountOfOption(HandRating.FourOfAKind, firstHit);
+        printAmountOfOption(HandRating.FullHouse, firstHit);
+        printAmountOfOption(HandRating.Flush, firstHit);
+        printAmountOfOption(HandRating.Straight, firstHit);
+        printAmountOfOption(HandRating.Triple, firstHit);
+//        printAmountOfOption(HandRating.Pair);
     }
 
     public static void main(String[] args) {
@@ -184,7 +233,7 @@ class Poker{
         }
 //        newPpker.print(card.type, newPpker.numArr.get(card.num));
         for (Card c: newPpker.cardsAndDeck) {
-            System.out.print(c.num + " " + c.type + ",");
+            System.out.print((c.num.ordinal() + 2) + " " + c.type + ",");
         }
         System.out.println();
 //        System.out.println("StraightFlash: " + poker.checkStraightFlash(cardList));
@@ -197,12 +246,15 @@ class Poker{
 //        newPpker.print();
 //        newPpker.checkOptions();
 
-        newPpker.printRateDeck(HandRating.StraightFlash);
-        newPpker.printRateDeck(HandRating.FourOfAKind);
-        newPpker.printRateDeck(HandRating.FullHouse);
-        newPpker.printRateDeck(HandRating.Flush);
-        newPpker.printRateDeck(HandRating.Straight);
-        newPpker.printRateDeck(HandRating.Triple);
-//        newPpker.printRateDeck(HandRating.Pair);
+
+        newPpker.sendToPrintEveryRate(true);
+        newPpker.addCard(newPpker.createRandomCard());
+        System.out.println("ADD ANOTHER CARD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        for (Card c: newPpker.cardsAndDeck) {
+            System.out.print((c.num.ordinal() + 2) + " " + c.type + ",");
+        }
+        System.out.println();
+        newPpker.sendToPrintEveryRate(false);
+//        newPpker.sendToPrintEveryCardOption();
     }
 }
